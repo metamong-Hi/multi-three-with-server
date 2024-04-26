8cf70cbce1dc660112f15f5908521def025415f9
metamong-Hi <seojinlee27@naver.com>
2024-04-26 13:41:12 +0900

    로그인 UI 추가

---
+++ client/src/LoginOverlay.jsx
@@ -0,0 +1,11 @@
+export default function LoginOverlay(){
+    return <>
+        <div className="login-layout">
+            <div>
+                <div className="name">YOUR NAME</div>
+                <input/>
+            </div>
+            <div className="login">JOIN</div>
+        </div>
+    </>
+}
